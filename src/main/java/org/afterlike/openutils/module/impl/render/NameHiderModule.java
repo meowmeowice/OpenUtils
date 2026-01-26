7cfae0aa844f6161f6d52c608504d5a42babe5b6
polaris <dev@afterlike.org>
2026-01-25 22:22:17 -0500

    feat: impl NameHiderModule

--- src/main/java/org/afterlike/openutils/module/impl/render/NameHiderModule.java
+++ src/main/java/org/afterlike/openutils/module/impl/render/NameHiderModule.java
@@ -2,12 +2,31 @@ package org.afterlike.openutils.module.impl.render;
 
 import org.afterlike.openutils.module.api.Module;
 import org.afterlike.openutils.module.api.ModuleCategory;
-import org.afterlike.openutils.module.api.setting.impl.DescriptionSetting;
+import org.afterlike.openutils.module.api.setting.impl.TextFieldSetting;
+import org.afterlike.openutils.util.client.ClientUtil;
+import org.afterlike.openutils.util.client.TextUtil;
 
 public class NameHiderModule extends Module {
-	private final DescriptionSetting todo;
+	private final TextFieldSetting customName;
+	private String cachedUsername;
 	public NameHiderModule() {
 		super("Name Hider", ModuleCategory.RENDER);
-		todo = this.registerSetting(new DescriptionSetting("Not implemented yet"));
+		customName = this
+				.registerSetting(new TextFieldSetting("Custom Name", "&bYou", "Enter name..."));
+	}
+
+	public String replaceName(String text) {
+		if (!ClientUtil.notNull()) {
+			return text;
+		}
+		String username = mc.thePlayer.getName();
+		if (!username.equals(cachedUsername)) {
+			cachedUsername = username;
+		}
+		if (!text.contains(cachedUsername)) {
+			return text;
+		}
+		String replacement = TextUtil.replaceColorCodes(customName.getValue());
+		return text.replace(cachedUsername, replacement);
 	}
 }
--- src/main/java/org/afterlike/openutils/platform/mixin/minecraft/gui/FontRendererMixin.java
+++ src/main/java/org/afterlike/openutils/platform/mixin/minecraft/gui/FontRendererMixin.java
@@ -3,6 +3,7 @@ package org.afterlike.openutils.platform.mixin.minecraft.gui;
 import net.minecraft.client.gui.FontRenderer;
 import org.afterlike.openutils.OpenUtils;
 import org.afterlike.openutils.module.impl.render.AntiShuffleModule;
+import org.afterlike.openutils.module.impl.render.NameHiderModule;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -18,6 +19,10 @@ public class FontRendererMixin {
 				&& string.contains("§k")) {
 			string = string.replace("§k", "");
 		}
+		if (OpenUtils.get().getModuleHandler().isEnabled(NameHiderModule.class)) {
+			string = OpenUtils.get().getModuleHandler().getModuleClass(NameHiderModule.class)
+					.replaceName(string);
+		}
 		return string;
 	}
 
@@ -30,6 +35,10 @@ public class FontRendererMixin {
 		if (OpenUtils.get().getModuleHandler().isEnabled(AntiShuffleModule.class)
 				&& string.contains("§k")) {
 			string = string.replace("§k", "");
+		}
+		if (OpenUtils.get().getModuleHandler().isEnabled(NameHiderModule.class)) {
+			string = OpenUtils.get().getModuleHandler().getModuleClass(NameHiderModule.class)
+					.replaceName(string);
 		}
 		return string;
 	}
